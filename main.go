5656636d0c36c0d17f5f3d1a182c0e56fb525522
Kagiri11 <charlesmaish423@gmail.com>
2022-01-17 01:19:11 +0300

    made use of for range construct to iterate a map

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	fmt.Println(isDayInWeek("frf"))
+}
+
+// use maps to hold the days of the week
+// for range construct
+func isDayInWeek(nameOfDay string) bool {
+	days := []string{"Sunday", "Monday", "Tuesday", "Wdenesday", "Thursday", "Friday", "Saturday"}
+	mapOfDays := make(map[string]int)
+	for index, day := range days {
+		mapOfDays[day] = index
+	}
+	fmt.Println(days)
+	_, presence := mapOfDays[nameOfDay]
+	return presence
 }
